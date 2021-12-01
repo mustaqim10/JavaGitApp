3b0f670c298d3b7616234a8448a3f2d828c42593
Mustaqim Kaderia <mustaqimkaderia@gmail.com>
2021-12-01 15:10:32 +0530

    second commit!

--- JavaGitApp/src/main/java/com/lti/demo/ClientApp.java
+++ JavaGitApp/src/main/java/com/lti/demo/ClientApp.java
@@ -5,5 +5,6 @@ public class ClientApp {
 	public static void main(String[] args) {
 		Product p1 = new Product (101,"Mobile");
 		System.out.println(p1);
+		System.out.println("hello World");
 	}
 }
